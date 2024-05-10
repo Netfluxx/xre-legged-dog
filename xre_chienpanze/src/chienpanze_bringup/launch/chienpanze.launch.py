75d77b51882dc795d02d34b7a18f93dc250429e9
Arno <arno.blan334@gmail.com>
2024-05-10 22:07:35 +0200

    compiles and the launch file seems to work

--- xre_chienpanze/src/chienpanze_bringup/launch/chienpanze.launch.py
+++ xre_chienpanze/src/chienpanze_bringup/launch/chienpanze.launch.py
@@ -1,207 +1,85 @@
-# $LICENSE$
-
 from launch import LaunchDescription
-from launch.actions import (
-    DeclareLaunchArgument,
-    RegisterEventHandler,
-    TimerAction,
-)
-from launch.event_handlers import OnProcessExit, OnProcessStart
-from launch.substitutions import (
-    Command,
-    FindExecutable,
-    LaunchConfiguration,
-    PathJoinSubstitution,
-)
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, TimerAction
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, FindExecutable
+from launch_ros.substitutions import FindPackageShare  # Corrected import
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-import launch_ros.descriptions
+from launch.event_handlers import OnProcessExit, OnProcessStart
 
+#import os
+#from ament_index_python.packages import get_package_share_directory
+
+from launch_ros.descriptions import ParameterValue
 
 def generate_launch_description():
-    # Declare arguments
-    declared_arguments = []
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "runtime_config_package",
-            default_value="chienpanze_bringup",
-            description='Package with the controller\'s configuration in "config" folder. \
-        Usually the argument is not set, it enables use of a custom setup.',
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "controllers_file",
-            default_value="chienpanze_controllers.yaml",
-            description="YAML file with the controllers configuration.",
-        )
-    )
-    declared_arguments.append(
+    # Setup launch arguments
+    declared_arguments = [
         DeclareLaunchArgument(
             "description_package",
             default_value="chienpanze_description",
-            description="Description package with robot URDF/xacro files. Usually the argument \
-        is not set, it enables use of a custom description.",
-        )
-    )
-    declared_arguments.append(
+            description="Package with robot description (URDF)."
+        ),
         DeclareLaunchArgument(
             "description_file",
             default_value="chienpanze.urdf.xacro",
-            description="URDF/XACRO description file with the robot.",
-        )
-    )
-    declared_arguments.append(
+            description="URDF/XACRO description file with the robot."
+        ),
         DeclareLaunchArgument(
-            "prefix",
-            default_value='""',
-            description="Prefix of the joint names, useful for \
-        multi-robot setup. If changed than also joint names in the controllers' configuration \
-        have to be updated.",
-        )
-    )
-    declared_arguments.append(
+            "runtime_config_package",
+            default_value="chienpanze_bringup",
+            description="Package with the runtime configuration."
+        ),
         DeclareLaunchArgument(
-            "use_mock_hardware",
-            default_value="true",
-            description="Start robot with fake hardware mirroring command to its states.",
+            "controllers_file",
+            default_value="chienpanze_controllers.yaml",
+            description="YAML file with the controller configurations."
         )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "mock_sensor_commands",
-            default_value="false",
-            description="Enable fake command interfaces for sensors used for simple simulations. \
-            Used only if 'use_mock_hardware' parameter is true.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "robot_controller",
-            default_value="displacement_controller",
-            choices=["forward_position_controller", "joint_trajectory_controller"],
-            description="Robot controller to start.",
-        )
-    )
+    ]
 
-    # Initialize Arguments
-    runtime_config_package = LaunchConfiguration("runtime_config_package")
-    controllers_file = LaunchConfiguration("controllers_file")
+    # Configure robot description
     description_package = LaunchConfiguration("description_package")
     description_file = LaunchConfiguration("description_file")
-    #prefix = LaunchConfiguration("prefix")
-    #use_mock_hardware = LaunchConfiguration("use_mock_hardware")
-    #mock_sensor_commands = LaunchConfiguration("mock_sensor_commands")
-    robot_controller = LaunchConfiguration("robot_controller")
-
-    # Get URDF via xacro
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
+    robot_description_content = ParameterValue(
+        Command([
+            FindExecutable(name="xacro"),
             " ",
-            PathJoinSubstitution(
-                [FindPackageShare(description_package), "urdf", description_file]
-            )
-        ]
+            PathJoinSubstitution([
+                FindPackageShare(description_package), "urdf", description_file
+            ])
+        ]),
+        value_type=str
     )
 
-    robot_description = {"robot_description": robot_description_content}
+    # Controller configurations
+    controllers_file = LaunchConfiguration("controllers_file")
+    runtime_config_package = LaunchConfiguration("runtime_config_package")
+    robot_controllers = PathJoinSubstitution([
+        FindPackageShare(runtime_config_package), "config", controllers_file
+    ])
 
-    robot_controllers = PathJoinSubstitution(
-        [FindPackageShare(runtime_config_package), "config", controllers_file]
-    )
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare(description_package), "rviz", "chienpanze.rviz"]
-    )
-
+    # Nodes
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
         output="both",
-        parameters=[robot_description, robot_controllers],
+        parameters=[{'robot_description': robot_description_content}, robot_controllers]
     )
     robot_state_pub_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
-        parameters=[robot_description],
-    )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
+        parameters=[{'robot_description': robot_description_content}]
     )
 
+    # Start the joint state broadcaster
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=[
-            "joint_state_broadcaster",
-            "--controller-manager",
-            "/controller_manager",
-        ],
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
     )
 
-    robot_controllers = [robot_controller]
-    robot_controller_spawners = []
-    for controller in robot_controllers:
-        robot_controller_spawners += [
-            Node(
-                package="controller_manager",
-                executable="spawner",
-                arguments=[controller, "-c", "/controller_manager"],
-            )
-        ]
-
-    # Delay loading and activation of `joint_state_broadcaster` after start of ros2_control_node
-    delay_joint_state_broadcaster_spawner_after_ros2_control_node = (
-        RegisterEventHandler(
-            event_handler=OnProcessStart(
-                target_action=control_node,
-                on_start=[
-                    TimerAction(
-                        period=1.0,
-                        actions=[joint_state_broadcaster_spawner],
-                    ),
-                ],
-            )
-        )
-    )
-
-    # Delay rviz start after Joint State Broadcaster to avoid unnecessary warning output.
-    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[rviz_node],
-        )
-    )
-
-    # Delay loading and activation of robot_controller after `joint_state_broadcaster`
-    delay_robot_controller_spawners_after_joint_state_broadcaster_spawner = []
-    for controller in robot_controller_spawners:
-        delay_robot_controller_spawners_after_joint_state_broadcaster_spawner += [
-            RegisterEventHandler(
-                event_handler=OnProcessExit(
-                    target_action=joint_state_broadcaster_spawner,
-                    on_exit=[
-                        TimerAction(
-                            period=3.0,
-                            actions=[controller],
-                        ),
-                    ],
-                )
-            )
-        ]
-
-    return LaunchDescription(
-        declared_arguments
-        + [
-            control_node,
-            robot_state_pub_node,
-            delay_rviz_after_joint_state_broadcaster_spawner,
-            delay_joint_state_broadcaster_spawner_after_ros2_control_node,
-        ]
-        + delay_robot_controller_spawners_after_joint_state_broadcaster_spawner
-    )
+    # Return the launch description
+    return LaunchDescription(declared_arguments + [
+        control_node,
+        robot_state_pub_node,
+        joint_state_broadcaster_spawner
+    ])
